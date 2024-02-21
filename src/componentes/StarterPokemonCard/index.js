21f8d535f31ed2a15c3c96d7db74361d45ce8293
Matheus <math.leal14@gmail.com>
2024-02-21 16:47:27 -0300

    Retomando código antigo para Home

--- src/componentes/StarterPokemonCard/index.js
+++ src/componentes/StarterPokemonCard/index.js
@@ -1,11 +1,53 @@
 import styled from 'styled-components'
+import pikachu from '../../imagens/pikachu.png'
+import bulbasaur from '../../imagens/bulbasaur.png'
+import charmander from '../../imagens/charmander.png'
+import squirtle from '../../imagens/squirtle.png'
 import { Link } from "react-router-dom"
 
 const StarterContainer = styled.div ``
 
 const html = document.querySelector('html')
 
-export default function StarterPokemonCards(props) {
+function starterPokemonCard() {
+    return(
+        <StarterContainer>
+            <Link className='inicial-home' to="/sobre" onClick={CharmanderCor}>
+                <img src= {charmander} alt='charmander'/>
+            </Link>
+            <Link className='inicial-home' to="/sobre" onClick={BulbasaurCor}>
+                <img src= {bulbasaur} alt='bulbasaur' />
+            </Link>
+            <Link className='inicial-home' to="/sobre" onClick={PikachuCor}>
+                <img src= {pikachu} alt='pikachu' />
+            </Link>
+            <Link className='inicial-home' to="/sobre" onClick={SquirtleCor}>
+                <img src= {squirtle} alt='squirtle' />
+            </Link>
+        </StarterContainer>
+    )
+    }
+    
+    function CharmanderCor() {
+        html.setAttribute('data-pokemon', 'charmander')
+    }
+    
+    function BulbasaurCor() {
+        html.setAttribute('data-pokemon', 'bulbasaur')
+    }
+    
+    function PikachuCor() {
+            html.setAttribute('data-pokemon', 'pikachu')
+    }
+    
+    function SquirtleCor() {
+        html.setAttribute('data-pokemon', 'squirtle')
+    }
+    
+    export default starterPokemonCard
+
+
+/* export default function StarterPokemonCards(props) {
     console.log(props)
     return(
         <StarterContainer>
@@ -27,4 +69,4 @@ function StarterPokemonCard({pokemon}) {
 
 function SetPokemonColor(pokemon) {
     html.setAttribute('data-pokemon', pokemon)
-}
+} */
